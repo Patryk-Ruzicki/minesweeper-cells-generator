6a0693c1c1cc06a79da33e5e31c924596bacd1f2
ArcySanta <87712798+Patryk-Ruzicki@users.noreply.github.com>
2023-03-01 11:48:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <random>
 
 using namespace std;
 
@@ -8,34 +7,32 @@ int main()
     
     srand(time(NULL));
     
-    int max = 5, min = 0, bPosY, bPosX, bombsAmount = 20, random;
+    int bombPosY, bombPosX, boardX = 12, boardY = 12, bombsAmount = 20, random;
     
-    int testXY[12][12] = {};
+    int testXY[boardY][boardX] = {};
     
     //set bombs randomly
-    for(int i = 1; i <= 10; i++){
-        for(int j = 1; j <= 10; j++){
-            if(bombsAmount > 1){
-                random = (rand()%(max + min)) + min;
-                if(random == 0){
-                    
-                    //set increment around the bomb
-                        testXY[i-1][j-1] += 1;
-                        testXY[i-1][j] += 1;
-                        testXY[i-1][j+1] += 1;
-                        testXY[i][j-1] += 1;
-                        testXY[i][j] = 99;
-                        testXY[i][j+1] += 1;
-                        testXY[i+1][j-1] += 1;
-                        testXY[i+1][j] += 1;
-                        testXY[i+1][j+1] += 1;
-                        
-                        bombsAmount--;
-                    //cout << "[" << bombsAmount << "]" << endl; //test line
-                }
+    do{
+        bombPosX = (rand() % 10) + 1;
+        bombPosY = (rand() % 10) + 1;
+        
+        if(testXY[bombPosY][bombPosX] < 10){
+            
+            //set increment around the bomb
+            
+            testXY[bombPosY][bombPosX] = 88;
+            
+            testXY[bombPosY][bombPosX-1] += 1;
+            testXY[bombPosY][bombPosX+1] += 1;
+            
+            for(int i = -1; i < 2; i++){
+                testXY[bombPosY-1][bombPosX+i] += 1;
+                testXY[bombPosY+1][bombPosX+i] += 1;
             }
+            bombsAmount--;
         }
-    }
+        
+    }while(bombsAmount > 0);
     
     
     //display cells
@@ -44,7 +41,6 @@ int main()
             cout << testXY[i][j] << "\t";
         cout << endl;
     }
-    
     
     return 0;
 }
